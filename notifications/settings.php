c3ad2334f0069363d1162baf937dddaeeef7b375
Robert Schrenk <robert@schrenk.cc>
2024-06-04 16:44:59 +0200

    #12 bugfix

--- notifications/settings.php
+++ notifications/settings.php
@@ -21,7 +21,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace local_displace\competency;
+namespace local_displace\notifications;
 defined('MOODLE_INTERNAL') || die;
 
 class settings {
